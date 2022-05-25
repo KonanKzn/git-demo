70942979fa7c7e4f1073220c988e3bd7ed8a2e39
KonanKzn <106175059+KonanKzn@users.noreply.github.com>
2022-05-25 19:13:22 +0300

    added more lines to the 1st file

--- int_input.py
+++ int_input.py
@@ -10,3 +10,16 @@ while True:
     except ValueError:
         print("Invalid value.", end = " ")
 print("cost:", cost)
+
+'''
+# from internet
+while True:
+    try:
+        numb = int(input("Enter the number:"))
+    except ValueError:
+        continue # продолжнает цикл; break  выводит с цикла
+    if numb % 2 == 0:
+        print("this number is EVEN")
+    elif numb % 2 != 0:
+        print("this number is ODD")
+'''
