55c7063d86417ee343264d16d5609e86cb9a4aa2
Kostiantyn Kuzin <konan.kuzin@gmail.com>
2022-05-25 18:45:34 +0300

    added 1st file

---
+++ int_input.py
@@ -0,0 +1,12 @@
+﻿# KZN revision
+while True: 
+    try:
+        cost = int(input("Enter the number:"))        
+        if cost <= 0:
+            print("Invalid value.", end = " ")
+            continue
+        else:
+            break    
+    except ValueError:
+        print("Invalid value.", end = " ")
+print("cost:", cost)
